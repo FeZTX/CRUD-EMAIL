e5bb6648a64c72f87b87011ed4824b50ab595fc5
Felipe Teixeira do Carmo <felipe_carmo@live.com>
2023-02-24 12:33:38 -0300

    Listagem funcionando

--- index.js
+++ index.js
@@ -1,47 +1,90 @@
 const btnInicia = document.getElementById("btnInicia");
 const container = document.getElementById("container");
+const btnLista = document.getElementById("btnLista");
 // const inputEmail = document.getElementById("")
+let id = 1;
 let emails = [];
+
+boxInput = document.createElement("div");
+inputEmail = document.createElement("Input");
+inputSenha = document.createElement("Input");
+boxButton = document.createElement("div");
+btnCria = document.createElement("button");
+btnVolta = document.createElement("button");
 
 function addMail() {
   container.innerHTML = `<h1>Crie Seu email!</h1>`;
 
-  boxInput = document.createElement("div");
+  inputEmail.value = "";
+  inputSenha.value = "";
+  boxInput.innerHTML = "";
+  boxButton.appendChild(btnVolta);
+  btnVolta.innerHTML = "Voltar";
+  btnVolta.setAttribute("onClick", "menu()");
+  boxInput.classList.remove("boxLista");
   boxInput.classList.add("boxInput");
-  inputEmail = document.createElement("Input");
   inputEmail.setAttribute("id", "inputEmail");
   inputEmail.setAttribute("type", "Email");
-  inputSenha = document.createElement("Input");
   inputSenha.setAttribute("id", "inputSenha");
   inputSenha.setAttribute("type", "Password");
-  boxButton = document.createElement("div");
   boxButton.classList.add("boxButton");
-  btnCria = document.createElement("button");
   btnCria.innerHTML = "Enviar";
-  btnCria.setAttribute("onClick", "teste()");
-
+  btnCria.setAttribute("onClick", "insereEmail()");
+  
   container.appendChild(boxInput);
   container.appendChild(boxButton);
   boxInput.appendChild(inputEmail);
   boxInput.appendChild(inputSenha);
   boxButton.appendChild(btnCria);
-  btnInicia.classList.add("ocultaBtn");
 }
 
-function teste() {
+function insereEmail() {
+  if(inputEmail.value && inputSenha.value != "") {
     user = {
-        email: inputEmail.value,
-        senha: inputSenha.value,
-    }
-
+      id: id,
+      email: inputEmail.value,
+      senha: inputSenha.value, 
+    };
     emails.push(user);
-    
+    id++;
+  }
 
-    console.log(emails);
-    btnInicia.classList.remove("ocultaBtn");
-    container.removeChild(boxButton);
-    container.removeChild(boxInput);
-    container.appendChild(btnInicia);
+  console.log(emails);
+  btnInicia.classList.remove("ocultaBtn");
+  container.removeChild(boxButton);
+  container.removeChild(boxInput);
+  container.appendChild(btnInicia);
+  container.appendChild(btnLista);
 }
 
-console.log(teste);
+function altera(){
+}
+
+function listaEmails() {
+  // alert("Ta funcionando pelo menos!");
+  
+  console.log(boxButton);
+  boxButton.innerHTML = "";
+  boxInput.innerHTML = "";
+
+  container.removeChild(btnInicia);
+  container.removeChild(btnLista);
+  container.appendChild(boxInput);
+  boxInput.classList.remove("boxInput");
+  boxInput.classList.add("boxLista");
+  container.appendChild(boxButton);
+  boxButton.classList.add("boxButton");
+  btnVolta.innerHTML = "Voltar";
+  boxButton.appendChild(btnVolta);
+  btnVolta.setAttribute("onClick", "menu()");
+
+  for(let i = 0; i <= emails.length; i++){
+    boxInput.innerHTML += `<span>${emails[i].id}- ${emails[i].email}</span>`;
+  }
+}
+
+function menu() {
+  container.innerHTML = "";
+  container.appendChild(btnInicia);
+  container.appendChild(btnLista);
+}
